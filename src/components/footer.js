8ef7f337a0030ddf7f00f17ff4a875136b4c0577
AdithyaGopakumar <adithyagopa@gmail.com>
2022-11-23 20:03:43 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,345 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="footer">
+        <div class="footer-container">
+          <section class="footer-top">
+            <div class="footer-left">
+              <ul class="footer-col">
+                <li>
+                  <span class="footer-col-heading">HEALTHKART</span>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Refer & Earn
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Loyalty Program
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Loyalty
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Blogs, Videos & More
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Brand Directory
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Authenticity Guaranteed
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Careers
+                  </a>
+                </li>
+              </ul>
+              <ul class="footer-col">
+                <li>
+                  <span class="footer-col-heading">BRANDS</span>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    MuscleBlaze
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Fit Foods
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    HK Vitals
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    TrueBasics
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Gritzo
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    bGREEN
+                  </a>
+                </li>
+              </ul>
+              <ul class="footer-col">
+                <li>
+                  <span class="footer-col-heading">HEALTH & FITNESS</span>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Bodybuilding
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Hair & Skin Care
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Sports Nutrition
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Vitamins & Supplements
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Ayurveda & Herbs
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Health Food & Drinks
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Fitness
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Wellness
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Consult Plans
+                  </a>
+                </li>
+              </ul>
+              <ul class="footer-col">
+                <li>
+                  <span class="footer-col-heading">POLICIES</span>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Terms & Conditions
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Delivery Policy
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Returns Policy
+                  </a>
+                </li>
+              </ul>
+              <ul class="footer-col">
+                <li>
+                  <span class="footer-col-heading">QUICK LINKS</span>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    My Account
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Track Your Order
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Store Locator
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    HK Cash
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    HK Coupons
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    FAQs
+                  </a>
+                </li>
+                <li>
+                  <a class="footer-links" href="#">
+                    Sell On HealthKart
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="footer-col last-footer-col">
+              <span class="footer-col-heading footer-side-heading">
+                ABOUT HEALTHKART
+              </span>
+              <span class="footer-text">
+                HealthKart.com is India’s largest online health & fitness store
+                for men and women. Shop online from the latest collections of
+                health, fitness and similar products featuring the best brands.
+              </span>
+              <span class="footer-col-heading footer-side-heading">
+                SUBSCRIBE TO OUR NEWSLETTER
+              </span>
+              <span class="footer-text">
+                Be the first one to receive amazing offers, deals and news
+              </span>
+              <form action=""></form>
+              <div class="contacts">
+                <p class="contact-heading">CONNECT WITH US</p>
+                <div class="contact-links-container">
+                  <a class="contact-link" href="#">
+                    <ion-icon
+                      class="contact-icon"
+                      name="logo-facebook"
+                    ></ion-icon>
+                  </a>
+                  <a class="contact-link" href="#">
+                    <ion-icon
+                      class="contact-icon"
+                      name="logo-instagram"
+                    ></ion-icon>
+                  </a>
+                  <a class="contact-link" href="#">
+                    <ion-icon
+                      class="contact-icon"
+                      name="logo-linkedin"
+                    ></ion-icon>
+                  </a>
+                  <a class="contact-link" href="#">
+                    <ion-icon
+                      class="contact-icon"
+                      name="logo-twitter"
+                    ></ion-icon>
+                  </a>
+                  <a class="contact-link" href="#">
+                    <ion-icon
+                      class="contact-icon"
+                      name="logo-youtube"
+                    ></ion-icon>
+                  </a>
+                  <a class="contact-link" href="#">
+                    <ion-icon
+                      class="contact-icon"
+                      name="logo-pinterest"
+                    ></ion-icon>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <hr />
+          <section class="footer-bottom">
+            <div class="seals">
+              <a href="#">
+                <img
+                  src="https://static1.hkrtcdn.com/hknext/static/media/common/footer/hk-consult.svg
+              "
+                  alt="HealthKart consult"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="https://static1.hkrtcdn.com/hknext/static/media/common/footer/A-G.svg"
+                  alt="Authenticity Guaranteed"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="	https://static1.hkrtcdn.com/hknext/static/media/common/footer/RP.svg
+              "
+                  alt="14 Day Return Policy"
+                />
+              </a>
+            </div>
+            <div class="disclaimer">
+              <span class="disclaimer-heading">Disclaimer:</span>
+              <span class="disclaimer-text">
+                The information contained on HealthKart (www.healthkart.com or
+                subdomains) is provided for informational purposes only and is
+                not meant to substitute for the advice provided by your doctor
+                or other healthcare professional. Information and statements
+                regarding products, supplements, programs etc listed on
+                HealthKart have not been evaluated by the Food and Drug
+                Administration or any government authority and are not intended
+                to diagnose, treat, cure, or prevent any disease. Please read
+                product packaging carefully prior to purchase and use. The
+                results from the products will vary from person to person. No
+                individual result should be seen as typical.
+              </span>
+            </div>
+          </section>
+          <hr />
+          <section class="copyright-download">
+            <div class="copyright-text">
+              Copyright &copy; 2022, HealthKart.com, or its affiliates
+            </div>
+            <div class="download">
+              <span class="download-text">DOWNLOAD OUR APP</span>
+              <div class="download-icon">
+                <a href="#">
+                  <img
+                    class="download-apple"
+                    src="	https://static1.hkrtcdn.com/hknext/static/media/common/footer/App-Store.svg
+              "
+                    alt="Available on the app store"
+                  />
+                </a>
+                <a href="#">
+                  <img
+                    class="download-android"
+                    src="	https://static1.hkrtcdn.com/hknext/static/media/common/footer/Google-Play.svg
+              "
+                    alt="Available on the app store"
+                  />
+                </a>
+              </div>
+            </div>
+          </section>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hai from react</h1>
+      <h1>Hai from react</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
